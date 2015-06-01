2f58805151f5207f36debc344133a5869d45d91f
Per Steffensen <steff@designware.dk>
2015-06-01 11:47:16 +0200

    Making DistributedQueryElevationComponentTest green, but removing previously made changes, which are not necessary anymore - actually the make the test fail

--- solr/core/src/test/org/apache/solr/handler/component/DistributedQueryElevationComponentTest.java
+++ solr/core/src/test/org/apache/solr/handler/component/DistributedQueryElevationComponentTest.java
@@ -28,8 +28,6 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import static org.apache.solr.client.solrj.embedded.JettySolrRunner.*;
-
 /**
  * 
  */
@@ -87,13 +85,13 @@ public class DistributedQueryElevationComponentTest extends BaseDistributedSearc
     handle.put("shards", SKIP);
     handle.put("q", SKIP);
     handle.put("qt", SKIP);
-    query(true, new String[]{"q", "*:*", "qt", "/elevate", "shards.qt", "/elevate", "rows", "500", "sort", "id desc", CommonParams.FL, "id, score, [elevated]"}, ALL_CREDENTIALS);
+    query("q", "*:*", "qt", "/elevate", "shards.qt", "/elevate", "rows", "500", "sort", "id desc", CommonParams.FL, "id, score, [elevated]");
 
-    query(true, new String[]{"q", "ZZZZ", "qt", "/elevate", "shards.qt", "/elevate", "rows", "500", CommonParams.FL, "*, [elevated]", "forceElevation", "true", "sort", "int_i desc"}, ALL_CREDENTIALS);
+    query("q", "ZZZZ", "qt", "/elevate", "shards.qt", "/elevate", "rows", "500", CommonParams.FL, "*, [elevated]", "forceElevation", "true", "sort", "int_i desc");
     
-    query(true, new String[]{"q", "solr", "qt", "/elevate", "shards.qt", "/elevate", "rows", "500", CommonParams.FL, "*, [elevated]", "forceElevation", "true", "sort", "int_i asc"}, ALL_CREDENTIALS);
+    query("q", "solr", "qt", "/elevate", "shards.qt", "/elevate", "rows", "500", CommonParams.FL, "*, [elevated]", "forceElevation", "true", "sort", "int_i asc");
     
-    query(true, new String[]{"q", "ZZZZ", "qt", "/elevate", "shards.qt", "/elevate", "rows", "500", CommonParams.FL, "*, [elevated]", "forceElevation", "true", "sort", "id desc"}, ALL_CREDENTIALS);
+    query("q", "ZZZZ", "qt", "/elevate", "shards.qt", "/elevate", "rows", "500", CommonParams.FL, "*, [elevated]", "forceElevation", "true", "sort", "id desc");
   }
   
   @Override
